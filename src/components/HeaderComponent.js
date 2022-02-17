845666dd0d1b5073256d9805bcec6abdfaaa17b0
youngwillyum <williamrchildres@gmail.com>
2022-02-17 12:51:45 -0600

    QuoteRequestModule
    added placeholders and a generate button. validations need work

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header(){
   return(
-    <div>Header Component</div>
+    <div>*headercomponent* QuoteZilla *headercomponent</div>
   )
 }
 
--- src/components/QuoterComponent.js
+++ src/components/QuoterComponent.js
@@ -5,6 +5,41 @@ function Quoter(){
   return(
     <React.Fragment>
     <div>Quoter</div>
+
+    <form>
+
+      <label>
+        Gallons Requested
+        <input type="text" name="gallons" />
+      </label>
+      <div/>
+
+      <label>
+        Delivery address - generated from client Profile
+      </label>
+      <div/>
+
+      <label>
+        Delivery Date - date picker
+      </label>
+      <div/>
+
+      <label>
+        Suggested price placeholder
+      </label>
+      <div/>
+
+      <label>
+        Total amount due:
+      </label>
+      <div/>
+
+      <input type="submit" value="Generate!" />
+
+
+    </form>
+
+
     <Link to='/home'>Click here for home</Link>
     </React.Fragment>
 )
