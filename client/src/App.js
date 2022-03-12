4c1fa0b152a376d094e0ead36ecf9b08c41f6b2d
youngwillyum <williamrchildres@gmail.com>
2022-03-12 16:10:30 -0600

    General Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from "react";
+import UserStore          from './stores/UserStore';
 import "bootstrap/dist/css/bootstrap.min.css"
-import Main from './components/MainComponent'
+import Main               from './components/MainComponent'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
-class App extends Component {
+
+class App extends React.Component {
+
+
   render(){
     return (
       <Router>
-        <div className="container">
+        <div className="app">
           <Main/>
         </div>
       </Router>
