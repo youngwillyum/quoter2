bf33b93451b859cd652d96e20273df81c7a4e10f
youngwillyum <williamrchildres@gmail.com>
2022-03-12 17:18:40 -0600

    Full Login System w Session Storage

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Main               from './components/MainComponent'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Login from './Login/Login';
+import useToken from './components/App/useToken';
 
 
 
 function App() {
- const [token, setToken] = useState();
+ const {token , setToken} = useToken();
   if(!token) {
     return <Login setToken={setToken}/>
   }
