8b863b975c967b860a226bdede768ee82b4fff44
youngwillyum <williamrchildres@gmail.com>
2022-03-11 19:31:27 -0600

    Profile Component
    Set  / Edit states implemented. Still having issues getting console to work and report profile inputs

--- client/src/components/ProfileComponent.js
+++ client/src/components/ProfileComponent.js
@@ -2,100 +2,193 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
 export default class Profile extends Component{
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    profile_fullname:'',
+    profile_address1:'',
+    profile_address2:'',
+    profile_city:'',
+    profile_state:'',
+    profile_zipcode:''
+  }
+
+  this.onChangeProfileName = this.onChangeProfileName.bind(this)
+  this.onChangeProfileAddress1 = this.onChangeProfileAddress1.bind(this)
+  this.onChangeProfileAddress2 = this.onChangeProfileAddress2.bind(this)
+  this.onChangeProfileCity = this.onChangeProfileCity.bind(this)
+  this.onChangeProfileState = this.onChangeProfileState.bind(this)
+  this.onChangeProfileZipcode = this.onChangeProfileZipcode.bind(this)
+
+
+}
+
+onChangeProfileName(e){
+  this.setState({
+    profile_fullname: e.target.value
+  });
+}
+
+onChangeProfileAddress1(e){
+  this.setState({
+    profile_address1: e.target.value
+  });
+}
+
+onChangeProfileAddress2(e){
+  this.setState({
+    profile_address2: e.target.value
+  });
+}
+
+onChangeProfileCity(e){
+  this.setState({
+    profile_city: e.target.value
+  });
+}
+
+onChangeProfileState(e){
+  this.setState({
+    profile_state: e.target.value
+  });
+}
+
+onChangeProfileZipcode(e){
+  this.setState({
+    profile_zipcode: e.target.value
+  });
+}
+
+onSubmit(e) {
+  e.preventDefault();
+
+  console.log('--Form Submitted--');
+  console.log(this.state.profile_fullname);
+  console.log('Profile Address1: ${this.state.profile_address1}');
+  console.log('Profile Address2: ${this.state.profile_address2}');
+  console.log('Profile City: ${this.state.profile_city}');
+  console.log('Profile State: ${this.state.profile_state}');
+  console.log('Profile Zipcode: ${this.state.profile_zipcode}');
+
+  this.state = ({
+    profile_fullname:'',
+    profile_address1:'',
+    profile_address2:'',
+    profile_city:'',
+    profile_state:'',
+    profile_zipcode:''
+  })
+}
+
+
+
+
+
+
+
 render(){
   return(
-    <React.Fragment>
-    <h2>Profile</h2>
-<form>
 
-  <label>
-    Full Name:
-    <input type="text" name="name" maxlength="50" required />
-  </label>
+      <React.Fragment>
+      <form onSubmit={this.onSubmit}>
 
-  <label>
-    Address 1:
-    <input type="text" address="address" maxlength="100" required/>
-  </label>
+        <div className="form-group">
+          <label>Full Name:</label>
+          <input type="text" className="form-control" maxlength="50" required value={this.state.profile_fullname}
+            onChange={this.onChangeProfileName}   />
+        </div>
 
-  <label>
-    Address 2:
-    <input type="text" address2="address2" maxlength="100" />
-  </label>
-  <div/>
+        <div className="form-group">
+          <label>Address 1:</label>
+          <input type="text" className="form-control" value={this.state.profile_address1}
+            onChange={this.onChangeProfileAddress1} maxlength="100" required/>
+        </div>
 
-  <label>
-    City:
-    <input type="text" City="City" maxlength="100" required/>
-  </label>
+        <div className="form-group">
+          <label>Address 2:</label>
+          <input type="text" className="form-control" value={this.state.profile_address2}
+            onChange={this.onChangeProfileAddress2} maxlength="100" />
+        </div>
 
-  <label>
-  State:
-  <select name="State" id="state" required>
-  <option value="">None</option>
-	<option value="AL">Alabama</option>
-	<option value="AK">Alaska</option>
-	<option value="AZ">Arizona</option>
-	<option value="AR">Arkansas</option>
-	<option value="CA">California</option>
-	<option value="CO">Colorado</option>
-	<option value="CT">Connecticut</option>
-	<option value="DE">Delaware</option>
-	<option value="DC">District Of Columbia</option>
-	<option value="FL">Florida</option>
-	<option value="GA">Georgia</option>
-	<option value="HI">Hawaii</option>
-	<option value="ID">Idaho</option>
-	<option value="IL">Illinois</option>
-	<option value="IN">Indiana</option>
-	<option value="IA">Iowa</option>
-	<option value="KS">Kansas</option>
-	<option value="KY">Kentucky</option>
-	<option value="LA">Louisiana</option>
-	<option value="ME">Maine</option>
-	<option value="MD">Maryland</option>
-	<option value="MA">Massachusetts</option>
-	<option value="MI">Michigan</option>
-	<option value="MN">Minnesota</option>
-	<option value="MS">Mississippi</option>
-	<option value="MO">Missouri</option>
-	<option value="MT">Montana</option>
-	<option value="NE">Nebraska</option>
-	<option value="NV">Nevada</option>
-	<option value="NH">New Hampshire</option>
-	<option value="NJ">New Jersey</option>
-	<option value="NM">New Mexico</option>
-	<option value="NY">New York</option>
-	<option value="NC">North Carolina</option>
-	<option value="ND">North Dakota</option>
-	<option value="OH">Ohio</option>
-	<option value="OK">Oklahoma</option>
-	<option value="OR">Oregon</option>
-	<option value="PA">Pennsylvania</option>
-	<option value="RI">Rhode Island</option>
-	<option value="SC">South Carolina</option>
-	<option value="SD">South Dakota</option>
-	<option value="TN">Tennessee</option>
-	<option value="TX">Texas</option>
-	<option value="UT">Utah</option>
-	<option value="VT">Vermont</option>
-	<option value="VA">Virginia</option>
-	<option value="WA">Washington</option>
-	<option value="WV">West Virginia</option>
-	<option value="WI">Wisconsin</option>
-	<option value="WY">Wyoming</option>
-  </select>
-  </label>
+        <div className="form-group">
+          <label>City:</label>
+          <input type="text"className="form-control"  value={this.state.profile_city}
+            onChange={this.onChangeProfileCity} maxlength="100" required/>
+        </div>
 
-  <label>
-    Zipcode:
-    <input type="text" zipcode="zipcode" maxlength="9" minlength="5" required/>
-  </label>
+        <div className="form-group">
+          <label>State:</label>
+          <select name="State" className="form-control" id="state"  value ={this.state.profile_state}
+          onChange={this.onChangeProfileState}required>
+          <option value="">None</option>
+	        <option value="AL">Alabama</option>
+	        <option value="AK">Alaska</option>
+	        <option value="AZ">Arizona</option>
+	        <option value="AR">Arkansas</option>
+	        <option value="CA">California</option>
+	        <option value="CO">Colorado</option>
+	        <option value="CT">Connecticut</option>
+	        <option value="DE">Delaware</option>
+	        <option value="DC">District Of Columbia</option>
+	        <option value="FL">Florida</option>
+	        <option value="GA">Georgia</option>
+	        <option value="HI">Hawaii</option>
+	        <option value="ID">Idaho</option>
+	        <option value="IL">Illinois</option>
+	        <option value="IN">Indiana</option>
+	        <option value="IA">Iowa</option>
+	        <option value="KS">Kansas</option>
+	        <option value="KY">Kentucky</option>
+	        <option value="LA">Louisiana</option>
+	        <option value="ME">Maine</option>
+	        <option value="MD">Maryland</option>
+	        <option value="MA">Massachusetts</option>
+	        <option value="MI">Michigan</option>
+	        <option value="MN">Minnesota</option>
+	        <option value="MS">Mississippi</option>
+	        <option value="MO">Missouri</option>
+	        <option value="MT">Montana</option>
+	        <option value="NE">Nebraska</option>
+	        <option value="NV">Nevada</option>
+	        <option value="NH">New Hampshire</option>
+	        <option value="NJ">New Jersey</option>
+	        <option value="NM">New Mexico</option>
+	        <option value="NY">New York</option>
+	        <option value="NC">North Carolina</option>
+	        <option value="ND">North Dakota</option>
+	        <option value="OH">Ohio</option>
+	        <option value="OK">Oklahoma</option>
+	        <option value="OR">Oregon</option>
+	        <option value="PA">Pennsylvania</option>
+	        <option value="RI">Rhode Island</option>
+	        <option value="SC">South Carolina</option>
+	        <option value="SD">South Dakota</option>
+	        <option value="TN">Tennessee</option>
+	        <option value="TX">Texas</option>
+	        <option value="UT">Utah</option>
+	        <option value="VT">Vermont</option>
+	        <option value="VA">Virginia</option>
+	        <option value="WA">Washington</option>
+	        <option value="WV">West Virginia</option>
+	        <option value="WI">Wisconsin</option>
+	        <option value="WY">Wyoming</option>
+          </select>
+        </div>
 
-  <input type="submit" value="Save Profile" />
+        <div className="form-group">
+          <label>Zipcode:  </label>
+          <input type="text" className="form-control" value={this.state.profile_zipcode}
+            onChange={this.onChangeProfileZipcode} maxlength="9" minlength="5" required/>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Save Profile" className = "btn btn-primary"/>
+        </div>
+
 </form>
-    <Link to='/home'>Click here for home</Link>
-    </React.Fragment>
+</React.Fragment>
 )
 }
 }
