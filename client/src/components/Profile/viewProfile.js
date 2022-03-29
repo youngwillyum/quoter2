3d085b3ed7290c87b4fb991557d6889a3a186e6f
Carsonmdial <carsonmdial@gmail.com>
2022-03-29 16:05:46 -0500

    more db changes
    changes to server file and viewprofile component

--- client/src/components/Profile/viewProfile.js
+++ client/src/components/Profile/viewProfile.js
@@ -6,9 +6,12 @@ const MyProfile = props => (
     <tr>
         <td>{props.profile.profile_fullname}</td>
         <td>{props.profile.profile_address1}</td>
+        <td>{props.profile.profile_address2}</td>
+        <td>{props.profile.profile_city}</td>
         <td>{props.profile.profile_state}</td>
+        <td>{props.profile.profile_zipcode}</td>
         <td>
-            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+            <Link to={"/edit/"+props.profile._id}>Edit</Link>
         </td>
     </tr>
 )
@@ -31,15 +34,16 @@ export default class ProfileInfo extends Component {
     }
 
 profileRead() {
-  return this.state.Profile.map(function(currentProfile, i){
+  return this.state.historys.map(function(currentProfile, i){
       return <MyProfile profile={currentProfile} key={i} />;
   })
 }
+
     render() {
         return (
             <div>
-            <h3>Profile Information</h3>
-            <table className="table table-striped" style={{ marginTop: 20 }} >
+                <h3>Profile Information</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
                 <thead>
                     <tr>
                         <th>Full Name</th>
@@ -51,10 +55,10 @@ profileRead() {
                     </tr>
                 </thead>
                 <tbody>
-                    { this.profRead() }
+                    { this.profileRead() }
                 </tbody>
             </table>
             </div>
-          )
+        )
     }
 }
